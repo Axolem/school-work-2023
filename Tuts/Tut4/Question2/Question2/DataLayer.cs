c9040a903c140dd5e1a5fc75fd12622ab45b11dc
Axole Maranjana <82832027+Axolem@users.noreply.github.com>
2023-04-27 15:40:42 +0200

    Add: Lab4

---
+++ Tuts/Tut4/Question2/Question2/DataLayer.cs
@@ -0,0 +1,85 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Question2
+{
+    internal class DataLayer
+    {
+        private static readonly string connectionString = "server=localhost;uid=root;password='';database=tutorial4";
+        private readonly MySqlConnection connection = new MySqlConnection(connectionString);
+        private MySqlCommand command;
+
+        //open and close the database connection
+        private void OpenCloseDBConnection()
+        {
+            switch (connection.State)
+            {
+                case ConnectionState.Closed:
+                    connection.Open();
+                    break;
+                default:
+                    connection.Close();
+                    break;
+            }
+
+        }
+
+        // method to insert values into database
+        public void SaveToDatabase(string name, string surname, string gender, string email)
+        {
+            string query = $"INSERT INTO users (name, surname, gender, email) values ('{name}','{surname}','{gender}','{email}')";
+
+            OpenCloseDBConnection();
+
+            command = new MySqlCommand(query, connection);
+            _ = command.ExecuteNonQuery();
+
+            OpenCloseDBConnection();
+        }
+
+        //method to read data from database
+        public DataTable ReadDataBase()
+        {
+            string query = "SELECT * FROM users";
+
+            OpenCloseDBConnection();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection);
+
+            DataTable dataTable = new DataTable();
+            _ = adapter.Fill(dataTable);
+
+            OpenCloseDBConnection();
+
+            return dataTable;
+        }
+
+        //mETHOD TO DELETE A ROW BU ID
+        public void DeleteRow(int id)
+        {
+            string query = $"DELETE FROM USERS WHERE id={id}";
+
+            OpenCloseDBConnection();
+
+            MySqlCommand command = new MySqlCommand(query, connection);
+            _ = command.ExecuteNonQuery();
+
+            OpenCloseDBConnection();
+
+        }
+
+        //METHOD TO UPDAE A ROW BY ID
+        public void UpdateRow(int id, string name, string surname, string gender, string email)
+        {
+            string query = $"UPDATE users SET name='{name}',surname='{surname}',gender='{gender}',email='{email}' WHERE id={id}";
+
+            OpenCloseDBConnection();
+
+            MySqlCommand command = new MySqlCommand(query, connection);
+            _ = command.ExecuteNonQuery();
+
+            OpenCloseDBConnection();
+
+        }
+    }
+}
---
+++ Tuts/Tut4/Question2/Question2/MainWindow.xaml.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Data;
+using System.Windows;
+
+namespace Question2
+{
+
+    public partial class MainWindow : Window
+    {
+        //Global variables
+        private string name, surname, gender, email;
+        private int id;
+
+        //Init new datalayer obj
+        private readonly DataLayer dataLayer = new DataLayer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Load data on form load
+            LoadData();
+        }
+
+        private void BTNdelete_Click(object sender, RoutedEventArgs e)
+        {
+            //Check id if id present
+            if (!AllFieldsFilled(true))
+            {
+                return;
+            }
+
+            //Delete the row
+            try
+            {
+                dataLayer.DeleteRow(id);
+                LoadData();
+                _ = MessageBox.Show($"Record {id} deleted successfuly!", "Success!");
+            }
+            catch (Exception ex)
+            {
+                _ = MessageBox.Show(ex.Message, "Delete Error!");
+            }
+        }
+
+        //Method to load data to the dataGrid
+        private void LoadData()
+        {
+            try
+            {
+                DataTable table = dataLayer.ReadDataBase();
+                GRDdisplay.ItemsSource = table.DefaultView;
+
+            }
+            catch (Exception ex)
+            {
+                _ = MessageBox.Show(ex.Message, "Loading Data Error!");
+            }
+        }
+
+
+        //Method to clear all field data
+        private void Clear()
+        {
+            name = surname = gender = email = "";
+            id = 0;
+            TXTemail.Clear();
+            TXTgender.Clear();
+            TXTid.Clear();
+            TXTname.Clear();
+            TXTsurname.Clear();
+        }
+
+
+        //method to check if user has entered all the data
+        public bool AllFieldsFilled(bool checkId)
+        {
+            if (checkId)
+            {
+                if (TXTid.Text == string.Empty)
+                {
+                    _ = MessageBox.Show("ID address required!");
+                    return false;
+                }
+                //Assing the values to the variables
+
+                try
+                {
+                    id = int.Parse(TXTid.Text);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    _ = MessageBox.Show(ex.Message, "Invalid Id Error!");
+                    return false;
+                }
+
+            }
+            else
+            {
+                if (TXTname.Text == string.Empty)
+                {
+                    _ = MessageBox.Show("Name required!");
+                    return false;
+                }
+                else if (TXTsurname.Text == string.Empty)
+                {
+                    _ = MessageBox.Show("Surname required!");
+                    return false;
+                }
+                else if (TXTgender.Text == string.Empty)
+                {
+                    _ = MessageBox.Show("Gender required!");
+                    return false;
+                }
+                else if (TXTemail.Text == string.Empty)
+                {
+                    _ = MessageBox.Show("Email address required!");
+                    return false;
+                }
+                else
+                {
+                    //Assing the values to the variables
+                    name = TXTname.Text;
+                    surname = TXTsurname.Text;
+                    gender = TXTgender.Text;
+                    email = TXTemail.Text;
+
+                    return true;
+                }
+
+
+            }
+
+        }
+
+        private void BTNclear_Click(object sender, RoutedEventArgs e)
+        {
+            Clear();
+        }
+
+        private void BTNinsert_Click(object sender, RoutedEventArgs e)
+        {
+            if (!AllFieldsFilled(false))
+            {
+                return;
+            }
+
+            try
+            {
+                dataLayer.SaveToDatabase(name, surname, gender, email);
+                LoadData();
+                _ = MessageBox.Show($"{name} saved successfully", "Success!");
+            }
+            catch (Exception ex)
+            {
+                _ = MessageBox.Show(ex.Message, "Save Error!");
+            }
+        }
+
+        private void BTNupdate_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (AllFieldsFilled(true) == false)
+            {
+                return;
+            }
+
+            if (AllFieldsFilled(false) == false)
+            {
+                return;
+            }
+
+            try
+            {
+                dataLayer.UpdateRow(id, name, surname, gender, email);
+                LoadData();
+                _ = MessageBox.Show($"{name} updated successfully", "Success!");
+            }
+            catch (Exception ex)
+            {
+                _ = MessageBox.Show(ex.Message, "Update Error!");
+            }
+        }
+    }
+}
